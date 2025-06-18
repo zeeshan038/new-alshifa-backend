069685bcf2cf4af0c82b1948ec74f84d3a837172
Muhammad Zeeshan <98173945+zeeshan038@users.noreply.github.com>
2025-06-18 10:28:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const index = require('./routes/index');
 const app = express();
 
 app.use(cors({
-  origin: "https://new-alshifa-frontend.vercel.app/", 
+  origin: "https://new-alshifa-frontend.vercel.app", 
   credentials: true
 }));
 app.use(express.json());
